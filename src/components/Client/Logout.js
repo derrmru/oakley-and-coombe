ecadc9ab273013cd263fe5f3c0801fa0494636b8
derrmru <podogomacbookair@MacBook-Air.local>
2021-10-05 17:20:20 +0200

    attempt logout without auth0 redirect

--- src/components/Client/Logout.js
+++ src/components/Client/Logout.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import { navigate } from 'gatsby'
 
 const Logout = () => {
     const { logout } = useAuth0()
     useEffect(() => {
-        let redirect = window.location.hostname !== 'localhost' ? 'https://https://oakleyandcoombemain.gatsbyjs.io/client/login' : 'http://localhost:8000/client/login'
-        logout({ returnTo: redirect })
+        logout()
+        navigate('/client/login')
+        return null
     })
     return (
         <div>Logging Out...</div>
