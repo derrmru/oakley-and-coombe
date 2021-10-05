dd50c0f4ff9a3a9a18fc3cf5ee76eea231030fa8
derrmru <podogomacbookair@MacBook-Air.local>
2021-10-05 17:07:04 +0200

    logout dynamic redirect

--- src/components/Client/Logout.js
+++ src/components/Client/Logout.js
@@ -4,7 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 const Logout = () => {
     const { logout } = useAuth0()
     useEffect(() => {
-        logout({ returnTo: "http://localhost:8000/client/login" })
+        logout({ returnTo: window.location.hostname !== 'localhost' ? 'https://https://oakleyandcoombemain.gatsbyjs.io/client/login' : "http://localhost:8000/client/login" })
     })
     return (
         <div>Logging Out...</div>
