360c7e63c39b64c1fa5cf41b64d25438a8c6a3db
derrmru <podogomacbookair@MacBook-Air.local>
2021-10-05 17:59:28 +0200

    hardcoded redirect

--- src/components/Client/Logout.js
+++ src/components/Client/Logout.js
@@ -4,7 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 const Logout = () => {
     const { logout } = useAuth0()
     useEffect(() => {
-        let redirect = window.location.hostname !== 'localhost' ? 'https://https://oakleyandcoombemain.gatsbyjs.io/client/login' : 'http://localhost:8000/client/login'
+        const redirect = 'https://https://oakleyandcoombemain.gatsbyjs.io/client/login'
         if (redirect) logout({ returnTo: redirect })
     })
     return (
